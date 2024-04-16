9819b461496ab0170fb6db09a55bfb67d49c95a1
Arik Bidny <arikbidny@gmail.com>
2024-04-16 18:45:48 +0300

    Add home route to Flask app

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 # import request
 from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({"message": "Hello, World!"})
+
+if __name__ == '__main__':
+    app.run(port=5000)
